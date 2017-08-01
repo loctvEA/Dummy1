5ca873f9d4d14e810194a6cfb4e97aa9a07c5629
Loc Truong <loc.truong@eastagile.com>
2017-08-01 12:16:16 +0700

    delete pyc, change urls, add views

--- BookStore/urls.py
+++ BookStore/urls.py
@@ -17,6 +17,8 @@ from django.conf.urls import url
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import test
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', test)
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
